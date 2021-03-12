257a17b3dfc85fb1917da32273ca212c23422d84
Vladyslav Okopnyy <harmfulcati@gmail.com>
2021-03-12 16:35:34 +0200

    Fine

--- js/script.js
+++ js/script.js
@@ -23,46 +23,52 @@
 // Esercizio pari o dispari
 
 var pairodd = prompt('Inserisci "pari" o "dispari":');
-var modality = document.getElementById('modality').innerHTML = 'Tu hai scelto modalita:' + ' ' + pairodd;
-// console.log('Tu hai scelto modalita:' + pairodd);
+if (pairodd === 'pari' || pairodd === 'dispari') {
+  var modality = document.getElementById('modality').innerHTML = 'Tu hai scelto modalita:' + ' ' + pairodd;
+  // console.log('Tu hai scelto modalita:' + pairodd);
 
-var number = parseInt(prompt('Inserisci un numero da 1 fino 5:'));
-var numberUser = document.getElementById('numberUser').innerHTML = 'Tu hai scelto numero:' + ' ' + number;
-// console.log('Tu hai scelto numero:' + number);
+  var number = parseInt(prompt('Inserisci un numero da 1 fino 5:'));
+  var numberUser = document.getElementById('numberUser').innerHTML = 'Tu hai scelto numero:' + ' ' + number;
+  // console.log('Tu hai scelto numero:' + number);
 
-var min = 1;
-var max = 5 - min;
-var rnd = Math.floor(Math.random() * max) + min;
-var numberPc = document.getElementById('numberPc').innerHTML = 'Il computer randomizzato numero:' + ' ' + rnd;
+  var min = 1;
+  var max = 5 - min;
+  var rnd = Math.floor(Math.random() * max) + min;
+  var numberPc = document.getElementById('numberPc').innerHTML = 'Il computer randomizzato numero:' + ' ' + rnd;
 
-var sum = number + rnd;
-var sumNumber = document.getElementById('sumNumber').innerHTML = 'La somma dei numeri e:' + ' ' + sum;
-// console.log('La somma dei numeri e:' + sum);
+  var sum = number + rnd;
+  var sumNumber = document.getElementById('sumNumber').innerHTML = 'La somma dei numeri e:' + ' ' + sum;
+  // console.log('La somma dei numeri e:' + sum);
 
-function ifpair() {
-    if (sum % 2 == 0) {
-        var risultWin = document.getElementById('risultWin').innerHTML = 'You Win';
-        // console.log('You Win');
-    } else if(sum % 2 == 1) {
-        var risultLose = document.getElementById('risultLose').innerHTML = 'You Lose';
-        // console.log('You Lose');
-    }
+  function ifpair() {
+      if (sum % 2 == 0) {
+          var risultWin = document.getElementById('risultWin').innerHTML = 'You Win';
+          // console.log('You Win');
+      } else if(sum % 2 == 1) {
+          var risultLose = document.getElementById('risultLose').innerHTML = 'You Lose';
+          // console.log('You Lose');
+      }
+  }
+
+  function ifodd() {
+      if (sum % 2 == 1) {
+          var risultWin = document.getElementById('risultWin').innerHTML = 'You Win';
+          // console.log('You Win');
+      }
+      else if(sum % 2 == 0) {
+          var risultLose = document.getElementById('risultLose').innerHTML = 'You Lose';
+          // console.log('You Lose');
+      }
+  }
+
+  if (pairodd == 'pari') {
+    ifpair();
+  }
+  else if (pairodd == 'dispari') {
+    ifodd();
+  }
+
 }
-
-function ifodd() {
-    if (sum % 2 == 1) {
-        var risultWin = document.getElementById('risultWin').innerHTML = 'You Win';
-        // console.log('You Win');
-    }
-    else if(sum % 2 == 0) {
-        var risultLose = document.getElementById('risultLose').innerHTML = 'You Lose';
-        // console.log('You Lose');
-    }
-}
-
-if (pairodd == 'pari') {
-  ifpair();
-}
-else if (pairodd == 'dispari') {
-  ifodd();
+else {
+  var errore = document.getElementById('errore').innerHTML = 'Caro giocatore, avete sbagliato di scegliere modalita di gioco, cliccare F5 è riprovate';
 }
